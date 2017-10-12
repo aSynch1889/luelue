a27c0cd9e4be79cddfcf11d3385d27258702f881
sunny华子 <aizi1989010@126.com>
2017-10-13 02:04:05 +0800

    1、新增进货单

---
+++ luelue/Classes/Home/Modules/purchaseID(进货单)/View/LLPurchaseIDTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  LLPurchaseIDTableViewCell.h
+//  luelue
+//
+//  Created by aaaa on 2017/10/13.
+//  Copyright © 2017年 aaaa Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLPurchaseIDTableViewCell : UITableViewCell
+
+@end
---
+++ luelue/Classes/Home/Modules/purchaseID(进货单)/View/LLPurchaseIDTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  LLPurchaseIDTableViewCell.m
+//  luelue
+//
+//  Created by aaaa on 2017/10/13.
+//  Copyright © 2017年 aaaa Co.,Ltd. All rights reserved.
+//
+
+#import "LLPurchaseIDTableViewCell.h"
+
+@implementation LLPurchaseIDTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
